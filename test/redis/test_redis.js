f94dfe1e39a5c5f177f50a824d7a563e430bf440
iot_pro <iot_pro@163.com>
2018-04-13 17:14:49 +0800

    create redis_test

---
+++ test/redis/test_redis.js
@@ -0,0 +1,44 @@
+const redis = require('redis');
+const client =redis.createClient(6379,"127.0.0.1");
+
+
+// client.keys('aa:*',function (err,keys) {
+//   console.time('keys');
+//   keys.forEach(key => client.del(key))
+//   console.timeEnd('keys');
+// });
+
+function del(){
+	console.time('keys');
+require('child_process').exec('redis-cli keys "aa:*" | xargs redis-cli del ', function(err, res) {
+    if (err) {
+        console.log(12,err);
+    }
+    console.log('res : ' + res);
+    console.timeEnd('keys');
+})
+
+}
+
+del();
+
+  
+
+
+
+function set(){
+	console.log(123)
+	for(let i=0;i<1000000;i++){
+		client.set('aa:'+i, i, function (res, reply) {
+		console.log('res:',res)
+		console.log('reply:',reply)
+	    });
+	}
+}
+
+ 
+// set();
+
+ 
+ 
+
