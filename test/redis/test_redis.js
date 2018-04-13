a0c782c3c9a072f8239f682d1d50a17a7157d7e1
iot_pro <iot_pro@163.com>
2018-04-13 17:16:19 +0800

    update redis_test

--- test/redis/test_redis.js
+++ test/redis/test_redis.js
@@ -1,23 +1,24 @@
 const redis = require('redis');
 const client =redis.createClient(6379,"127.0.0.1");
 
-
+//方法一：
 // client.keys('aa:*',function (err,keys) {
 //   console.time('keys');
 //   keys.forEach(key => client.del(key))
 //   console.timeEnd('keys');
 // });
 
+
+//方法二：
 function del(){
 	console.time('keys');
-require('child_process').exec('redis-cli keys "aa:*" | xargs redis-cli del ', function(err, res) {
+  require('child_process').exec('redis-cli keys "aa:*" | xargs redis-cli del ', function(err, res) {
     if (err) {
         console.log(12,err);
     }
     console.log('res : ' + res);
     console.timeEnd('keys');
-})
-
+  })
 }
 
 del();
@@ -25,7 +26,7 @@ del();
   
 
 
-
+//添加测试数据
 function set(){
 	console.log(123)
 	for(let i=0;i<1000000;i++){
